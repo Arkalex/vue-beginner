801dc6af675c94535e7abf42a405400b1ee23ef9
Alex <alexayllon1@hotmail.com>
2020-02-08 16:56:55 +0100

    init project

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hello World with Vue!',
+        frutas: [
+            {nombre: 'Pera', cantidad: 10},
+            {nombre: 'Manzana', cantidad: 0},
+            {nombre: 'platanos', cantidad: 11}
+        ]
+    }
+});
